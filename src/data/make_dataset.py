cfdf493344ee1ede1bfd6edbd215181d2561f48b
shenlinnn <slinda1992@gmail.com>
2019-07-08 02:23:01 +0800

    load & pre-process data to data/interim

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 import pandas_gbq
+import click
+from pathlib import Path
 
-def main():
+@click.command()
+@click.argument('output_filepath', type=click.Path())
+
+def main(output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -20,7 +22,7 @@ def main():
     # remove duplicates
     # handle records with same key but different values
     # fillna for empty driver_id of booking_log.booking_status=='CREATED'
-    
+
     ## train
     agg_booking = {'event_timestamp': 'min', 'trip_distance': 'mean', 'pickup_latitude': 'mean', 'pickup_longitude': 'mean'}
     booking = booking_log.drop_duplicates().fillna('').groupby(['order_id', 'booking_status', 'customer_id', 'driver_id'], as_index=False).agg(agg_booking)
@@ -28,23 +30,22 @@ def main():
     agg_driver = {'driver_latitude': 'mean', 'driver_longitude': 'mean', 'driver_gps_accuracy': 'mean'}
     driver = driver_log.drop_duplicates().groupby(['order_id', 'driver_id'], as_index=False).agg(agg_driver)
 
-    # test
+    ## test
     agg_test = {'event_timestamp': 'min', 'trip_distance': 'mean', 'pickup_latitude': 'mean', 'pickup_longitude': 'mean', 'driver_latitude': 'mean', 'driver_longitude': 'mean', 'driver_gps_accuracy': 'mean'}
-    test = test_data.drop_duplicates().groupby(['order_id', 'customer_id', 'driver_id'], as_index=False).agg(agg)
+    test = test_data.drop_duplicates().groupby(['order_id', 'customer_id', 'driver_id'], as_index=False).agg(agg_test)
+
+    #### writing to /data/interim
+    booking.to_csv(output_filepath + '/booking.csv', index=False)
+    driver.to_csv(output_filepath + '/driver.csv', index=False)
+    test.to_csv(output_filepath + '/test.csv', index=False)
+    
 
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('making interim data set from GCP raw data')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
